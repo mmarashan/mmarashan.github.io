bc0bc735c93b9eb8ad400ae5cd2a87a08d99a97b
Maksim Marashan <mmarashan@gmail.com>
2024-10-29 23:57:33 +0300

    Showcase of posts with image

--- blog-app/pages/index.js
+++ blog-app/pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import ShowcaseItem from '../components/showcaseItem';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
   return (
@@ -19,15 +18,11 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
+          {
+            allPostsData.map(({ id, date, title, poster }) => (
+              <ShowcaseItem id = {id} date = {date} title = {title} poster = {poster}/>
+            ))
+          }
         </ul>
       </section>
     </Layout>
