743aae38ddfc4fcbca0c18f8391fc8ae88688fd7
Maksim Marashan <mmarashan@gmail.com>
2024-11-11 08:47:29 +0300

    Hovering ImageListItemBar

--- blog-app/components/imagePalette.js
+++ blog-app/components/imagePalette.js
@@ -1,41 +1,43 @@
 import * as React from 'react';
+import { useState } from "react";
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import $ from 'jquery';
+
 
 export default function ImagePalette({items}) {
   return (
     <ImageList variant="masonry" cols={2} gap={8}>
       {items.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={item.poster}
-            alt={item.title}
-            loading="lazy"
-            onMouseOver={event => {onMouseOver(event, item.title)}}
-            onMouseOut={event => {onMouseOut(event, item.title)}}
-          />
-          
-          <ImageListItemBar
-            id = {item.title}
-            title={item.title}
-            subtitle={<span>{item.date}</span>}
-            // position="below"
-          />
-         
-        </ImageListItem>
+        <ImageItem item={item}/>
       ))}
     </ImageList>
   );
 }
 
-function onMouseOver(e, itemId){
-    console.log("onMouseOut "+ itemId);
-    return false;
-}
-
-function onMouseOut(e, itemId){
-    console.log("onMouseOut "+ itemId);
-    return false;
+function ImageItem({item}) { 
+    const [isHovering, setIsHovering] = useState(false);
+    return (
+      <ImageListItem key={item.img}>
+      <img
+        src={item.poster}
+        alt={item.title}
+        loading="lazy"
+        onMouseOver={event => {setIsHovering(true)}}
+        onMouseOut={event => {setIsHovering(false)}}
+      />
+      {isHovering &&
+      <ImageListItemBar
+        id = {item.title}
+        title={item.title}
+        subtitle={<span>{item.date}</span>}
+        sx={{
+          "& .MuiImageListItemBar-title": { fontSize: '14px' },
+          "& .MuiImageListItemBar-subtitle": { fontSize: '10px' },
+        }}
+      />
+      }
+     
+    </ImageListItem>
+    )
 }
