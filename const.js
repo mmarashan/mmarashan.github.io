ee0df53661bd04e1514553e3b32684d68c6f89d4
Maksim Marashan <mmarashan@gmail.com>
2024-11-18 14:06:09 +0300

    Updates

--- const.js
+++ const.js
@@ -1,10 +1,13 @@
 
 export const strings = {
     siteUrl: 'http://marashan.ru',
-    siteTitle: 'Yet another programmer\'s blog.',
-    myName: 'Максим Марашан 🚀',
-    myDescription: "👨🏻‍💻 Инженер-разработчик с продуктовым мышлением 💡 с опытом разработки fintech-приложений 🧰, голосовых ассистентов 🤖, корпоративного софта в legal 🛡️, funtech-приложений. 🎬",
+    siteTitle: '🚀 Yet another programmer\'s blog',
+    myName: 'Максим Марашан',
+    myDescription: "Инженер-разработчик с опытом разработки fintech-приложений 🧰, голосовых ассистентов 🤖, корпоративного софта в legal 🛡️, funtech-приложений 🎬",
     blogTitle: '📝 Блог',
     portfolioTitle: '👨🏻‍💻 Портфолио',
-    backToHome: '🏠 На главную'
+    backToHome: '🏠 На главную',
+    emailTitle: '📩 ',
+    myEmail: 'marashanmv@yandex.ru',
+    githubUrl: 'https://github.com/mmarashan'
 }
